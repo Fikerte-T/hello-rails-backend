8ce6e0c2feab46ff71b1831b973064a007b69705
Fikerte-T <tofikertesfaye@gmail.com>
2022-06-07 05:34:28 +0300

    Update cors configuration

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,11 +1,7 @@
 class V1::GreetingsController < ApplicationController
     def index
-      @greeting = Greeting.order('RANDOM()').first
-      if @greeting
-        render json: { success: true, message: 'Greeting found', data: { greeting: @greeting } }, status: :ok
-      else
-        render json: { success: false, errors: 'Greeting not found' }, status: :unprocessable_entity
-      end
+      @greeting = Greeting.all
+      render json: @greeting.sample
     end
   end
   
